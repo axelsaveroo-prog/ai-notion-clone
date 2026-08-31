e0c3321c6bfb090ef490d913e525b2752963c2f4
axelsaveroo-prog <axelsaveroo@gmail.com>
2026-09-01 02:57:59 +0700

    Remove comment in MobileContainer component
    Removed comment about fluid responsive container.

--- src/components/MobileContainer.tsx
+++ src/components/MobileContainer.tsx
@@ -3,7 +3,6 @@
 export function MobileContainer({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-[#0b0b0c] text-gray-100 flex flex-col selection:bg-orange-500 selection:text-white">
-      {/* Fluid responsive container: full width on mobile, max-width container centered on desktop */}
       <div className="w-full max-w-6xl mx-auto flex-1 flex flex-col px-4 sm:px-8 py-4 sm:py-8">
         {children}
       </div>
